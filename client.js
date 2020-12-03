5038a7bf370be46775ac955841ba052bbf010c4b
aitorjs <aitiba@gmail.com>
2020-12-03 02:21:58 +0100

    update client.js

--- client.js
+++ client.js
@@ -1,7 +1,18 @@
 const net = require('net');
+const readline = require('readline');
 
 const client = new net.Socket();
-client.connect({ port: 58901, host: "localhost" });
+client.connect(58901, process.argv[2], () => {
+    console.log('Connected to server');
+});
 client.on('data', (data) => {
-    console.log('data', data.toString('utf-8'));
+    console.log(data.toString('utf-8'));
+});
+
+const rl = readline.createInterface({ input: process.stdin });
+rl.on('line', (line) => {
+    client.write(`${line}\n`);
+});
+rl.on('close', () => {
+    client.end();
 });
